399456f752d6c9b92cbedede3be3b31fba1dd204
ivanchio97 <git@stackblitz.com>
2026-02-16 19:18:09 -0600

    Initial commit

---
+++ src/words.js
@@ -0,0 +1,61 @@
+export const words = [
+  { category: "frutas", palabra: "manzana" },
+  { category: "frutas", palabra: "platano" },
+  { category: "frutas", palabra: "naranja" },
+  { category: "frutas", palabra: "sandia" },
+  { category: "frutas", palabra: "fresa" },
+
+  { category: "animales", palabra: "perro" },
+  { category: "animales", palabra: "gato" },
+  { category: "animales", palabra: "elefante" },
+  { category: "animales", palabra: "jirafa" },
+  { category: "animales", palabra: "delfin" },
+
+  { category: "cine", palabra: "Titanic" },
+  { category: "cine", palabra: "Avatar" },
+  { category: "cine", palabra: "Toy Story" },
+  { category: "cine", palabra: "Jurassic Park" },
+  { category: "cine", palabra: "Matrix" },
+
+  { category: "videojuegos", palabra: "Minecraft" },
+  { category: "videojuegos", palabra: "Fortnite" },
+  { category: "videojuegos", palabra: "Roblox" },
+  { category: "videojuegos", palabra: "Zelda" },
+  { category: "videojuegos", palabra: "Mario Kart" },
+
+  { category: "paises", palabra: "Mexico" },
+  { category: "paises", palabra: "Brasil" },
+  { category: "paises", palabra: "España" },
+  { category: "paises", palabra: "Canada" },
+  { category: "paises", palabra: "Japon" },
+
+  { category: "objetos", palabra: "mesa" },
+  { category: "objetos", palabra: "silla" },
+  { category: "objetos", palabra: "telefono" },
+  { category: "objetos", palabra: "reloj" },
+  { category: "objetos", palabra: "mochila" },
+
+  { category: "comida", palabra: "pizza" },
+  { category: "comida", palabra: "tacos" },
+  { category: "comida", palabra: "hamburguesa" },
+  { category: "comida", palabra: "sushi" },
+  { category: "comida", palabra: "pasta" },
+
+  { category: "lugares", palabra: "escuela" },
+  { category: "lugares", palabra: "hospital" },
+  { category: "lugares", palabra: "playa" },
+  { category: "lugares", palabra: "cine" },
+  { category: "lugares", palabra: "parque" },
+
+  { category: "deportes", palabra: "futbol" },
+  { category: "deportes", palabra: "basquetbol" },
+  { category: "deportes", palabra: "tenis" },
+  { category: "deportes", palabra: "natacion" },
+  { category: "deportes", palabra: "boxeo" },
+
+  { category: "internet", palabra: "TikTok" },
+  { category: "internet", palabra: "memes" },
+  { category: "internet", palabra: "streamer" },
+  { category: "internet", palabra: "influencer" },
+  { category: "internet", palabra: "hashtag" }
+];
